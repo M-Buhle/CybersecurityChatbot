0066fd53441f0221a4d9afa09bc4e03732e4ca7d
M-Buhle <ST10488724@rcconnect.edu.za>
2026-04-13 12:08:50 +0200

    Added gitignore file

--- obj/Debug/net11.0/CybersecurityChatbot.AssemblyInfo.cs
+++ obj/Debug/net11.0/CybersecurityChatbot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CybersecurityChatbot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a79c4519065d162239af1989504c3de8c360333")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cba78d8d5af0d9ef30176c47250e1cc8535b5e83")]
 [assembly: System.Reflection.AssemblyProductAttribute("CybersecurityChatbot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CybersecurityChatbot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
